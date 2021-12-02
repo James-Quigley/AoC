4e4e152a3dcf1ff56889693c9bc8d8e8b0e063d0
James Quigley <james@quigley.us>
2021-12-01 22:10:04 -0700

    2021 day 2

--- 2021/2/index.ts
+++ 2021/2/index.ts
@@ -1,9 +1,45 @@
+import * as R from "ramda";
+
 const a = (input: string): string => {
-  return "";
+  let h = 0;
+  let depth = 0;
+
+  const lines = input.split("\n").map((line) => line.split(" "));
+
+  for (let [comm, unit] of lines) {
+    let d = parseInt(unit);
+    if (comm == "forward") {
+      h += d;
+      continue;
+    }
+    if (comm == "up") {
+      d *= -1;
+    }
+    depth += d;
+  }
+  return (depth * h).toString();
 };
 
 const b = (input: string): string => {
-  return "";
+  let h = 0;
+  let depth = 0;
+  let aim = 0;
+
+  const lines = input.split("\n").map((line) => line.split(" "));
+
+  for (let [comm, unit] of lines) {
+    let d = parseInt(unit);
+    if (comm == "forward") {
+      h += d;
+      depth += aim * d;
+      continue;
+    }
+    if (comm == "up") {
+      d *= -1;
+    }
+    aim += d;
+  }
+  return (depth * h).toString();
 };
 
 export default {
