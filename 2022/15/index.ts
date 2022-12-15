e22a83606250c8ba856227ab86eef94376223c10
James Quigley <james@quigley.us>
2022-12-15 17:44:54 +0000

    2022 Day 15 part 2

--- 2022/15/index.ts
+++ 2022/15/index.ts
@@ -35,74 +35,63 @@ const b = (input: string): string => {
 
     const maxCoord = isTest ? 20 : 4000000
 
-    const target: {
-        [key: string]: Set<number>
-    } = {}
-
+    const beacons = []
     for (let line of lines) {
         const matches = /Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)/.exec(line)
-        const [sensorX, sensorY, beaconX, beaconY] = matches.slice(1, 5).map(s => parseInt(s))
-        const manDist = Math.abs(sensorX - beaconX) + Math.abs(sensorY - beaconY)
-
-        for (let y = sensorY - manDist; y <= sensorY + manDist; y++) {
-            if (R.isNil(target[y])) {
-                target[y] = new Set()
-            }
-            const eitherSideWidth = manDist - Math.abs(sensorY - y)
-            for (let x = sensorX - eitherSideWidth; x <= sensorX + eitherSideWidth; x++) {
-                if (x >=0 && x <= maxCoord) {
-                    target[y].add(x)
-                }
-            }
-        }
-        if (sensorY >= 0 && sensorX >=0 && sensorY <= maxCoord && sensorX <= maxCoord) {
-            target[sensorY].add(sensorX)
-        }
-        if (beaconY >= 0 && beaconX >=0 && beaconY <= maxCoord && beaconX <= maxCoord) {
-            target[beaconY].add(beaconX)
-        }
+        beacons.push(matches.slice(1, 5).map(s => parseInt(s)))
     }
-
-    return "uh oh"
-}
-
-const b2 = (input: string): string => {
-    const lines = input.split("\n")
-    const isTest = lines[0] === "Sensor at x=2, y=18: closest beacon is at x=-2, y=15"
-
-    const maxCoord = isTest ? 20 : 4000000
 
     for (let y = 0; y <= maxCoord; y++) {
         const targetY = y
 
-        const target = {}
+        let ranges = []
     
-        for (let line of lines) {
-            const matches = /Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)/.exec(line)
-            const [sensorX, sensorY, beaconX, beaconY] = matches.slice(1, 5).map(s => parseInt(s))
+        for (let [sensorX, sensorY, beaconX, beaconY] of beacons) {
             const manDist = Math.abs(sensorX - beaconX) + Math.abs(sensorY - beaconY)
             if (sensorY - manDist < targetY && targetY < sensorY + manDist) {
                 const eitherSideWidth = manDist - Math.abs(sensorY - targetY)
-                for (let x = sensorX - eitherSideWidth; x <= sensorX + eitherSideWidth; x++) {
-                    if (x >=0 && x <= maxCoord) {
-                        target[x] = "#"
-                    }
-                }
+                ranges.push([sensorX - eitherSideWidth, sensorX + eitherSideWidth])
             }
             if (sensorY === targetY && sensorY >= 0 && sensorY <= maxCoord) {
-                target[sensorX] = "S"
+                ranges.push([sensorX, sensorX + 1])
             }
             if (beaconY === targetY && beaconY >=0 && beaconY <= maxCoord) {
-                target[beaconX] = "B"
+                ranges.push([beaconX, beaconX + 1])
             }
         }
 
-        const diff = R.difference(
-            R.range(0, maxCoord + 1),
-            Object.keys(target).map(s => parseInt(s))
-        )
-        if (diff.length) {
-            return ((diff[0] * 4000000) + targetY).toString()
+        // If union of ranges covers 0 -> maxCoord (inclusive)
+        // Then we can continue
+        ranges = ranges.sort((a, b) => a[0] - b[0])
+
+        const unioned = []
+        for (const [start, end] of ranges) {
+            if (unioned.length && R.last(unioned)[1] >= start - 1) {
+                unioned[unioned.length - 1][1] = Math.max(R.last(unioned)[1], end)
+            } else {
+                unioned.push([start, end])
+            }
+        }
+
+        let inRange = false
+        let idx = -1
+        for (let [start, end] of unioned) {
+            if (!inRange) {
+                if (end >= 0) {
+                    inRange = true
+                }
+            }
+
+            if (inRange) {
+                if (start <= 0 && end >= maxCoord) {
+                    break
+                }
+                idx = end + 1
+                break
+            }
+        }
+        if (idx !== -1) {
+            return ((idx * 4000000) + targetY).toString()
         }
     }
 
