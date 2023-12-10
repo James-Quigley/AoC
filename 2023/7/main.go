2331ccf4caf4665773d108d7d7ee830d64532c0e
James Quigley <james@quigley.us>
2023-12-10 12:48:49 -0500

    2023 Day 7 - Part 2

--- 2023/7/main.go
+++ 2023/7/main.go
@@ -149,5 +149,117 @@ func part1(data string) {
 
 }
 
+func part2GetCardValue(card string) int {
+	switch card {
+	case "A":
+		return 14
+	case "K":
+		return 13
+	case "Q":
+		return 12
+	case "J":
+		return 1
+	case "T":
+		return 10
+	default:
+		n, err := strconv.Atoi(card)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		return n
+	}
+}
+
+func part2GetHandType(cards string) int {
+	// Determine hand type
+	// 0: High card
+	// 1: Pair
+	// 2: Two pair
+	// 3: Three of a kind
+	// 4: Full House
+	// 5: Four of a kind
+	// 6: Five of a kind
+	// J counts as a wildcard
+
+	chars := strings.Split(cards, "")
+	sort.Strings(chars)
+
+	m := make(map[string]int)
+	for _, c := range chars {
+		m[c]++
+	}
+
+	jokers := m["J"]
+	delete(m, "J")
+
+	highestCount := 0
+	for _, v := range m {
+		if v > highestCount {
+			highestCount = v
+		}
+	}
+
+	newHighestCount := highestCount + jokers
+	if newHighestCount == 5 {
+		return 6
+	}
+	if newHighestCount == 4 {
+		return 5
+	}
+
+	// Check for full house
+	if newHighestCount == 3 && len(m) == 2 {
+		return 4
+	}
+
+	// Check for three of a kind
+	if newHighestCount == 3 {
+		return 3
+	}
+
+	// Check for two pair
+	if newHighestCount == 2 && len(m) == 3 {
+		return 2
+	}
+	// Check for pair
+	if newHighestCount == 2 {
+		return 1
+	}
+	return 0
+}
+
 func part2(data string) {
+	lines := strings.Split(data, "\n")
+	hands := make([]Hand, len(lines))
+	for i, line := range lines {
+		parts := strings.Split(line, " ")
+		bid, err := strconv.Atoi(parts[1])
+		if err != nil {
+			log.Fatalln(err)
+		}
+		hands[i] = Hand{
+			cards:    parts[0],
+			bid:      bid,
+			handType: part2GetHandType(parts[0]),
+		}
+	}
+
+	sort.Slice(hands, func(i, j int) bool {
+		if hands[i].handType == hands[j].handType {
+			for x := 0; x < len(hands[i].cards); x++ {
+				if hands[i].cards[x] == hands[j].cards[x] {
+					continue
+				}
+
+				return part2GetCardValue(string(hands[i].cards[x])) < part2GetCardValue(string(hands[j].cards[x]))
+			}
+		}
+		return hands[i].handType < hands[j].handType
+	})
+
+	total := 0
+	for i := 0; i < len(hands); i++ {
+		total += hands[i].bid * (i + 1)
+	}
+	log.Println(total)
 }
